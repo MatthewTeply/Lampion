325694c707e0fe2868edbd86a96d5293e42c7b57
Matyáš Teplý <info@matyasteply.eu>
2020-01-05 16:41:30 +0100

    - Fixed classes not loading on Unix systems

--- core/Core/Autoloader.php
+++ core/Core/Autoloader.php
@@ -12,7 +12,7 @@ class Autoloader
             $classNameExplode = explode("\\", $className);
 
             $app       = array_shift($classNameExplode);
-            $className = implode("\\", $classNameExplode);
+            $className = implode("/", $classNameExplode);
 
             # If core classes are called
             if($app == "Lampion") {
